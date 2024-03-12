c9fd70f33fb76d636f6c784d059fa1ce7127b00b
Ndue Krasniqi <ndue@91.life>
2024-03-12 15:29:25 +0100

    different env

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,16 +1,25 @@
 import { test } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
+import { log } from "console";
 
 test("test01", async({page})=>{ 
 
     const loginPage = new LoginPage(page);
 
     await loginPage.navigateToLoginPage();
-    await loginPage.enter_username("ndue@krasniqi.com");
-    await loginPage.enter_password("mars2024");
+    await loginPage.enter_username(process.env.userid!);
+    await loginPage.enter_password(process.env.password!);
     const homePage = await loginPage.clickLoginButton();
 
     await homePage.service_title_is_visible();
 
 
+});
+
+test("Simple env test", async({page})=>{ 
+ 
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.userid);
+    console.log(process.env.password);
+    
 });
